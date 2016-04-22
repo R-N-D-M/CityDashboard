64506ee25bf48a80f69b6d6ddd82534d59b11584
Dylan Hughes <dylanbhughes@gmail.com>
2016-04-22 15:10:26 -0700

    small update.

--- server.js
+++ server.js
@@ -9,12 +9,6 @@ var compiler = webpack(webpackConfig);
 
 var app = express();
 
-// app.use(express.static(path.join(__dirname, 'client/')));
-
-////////////////////////////////////////////////////////////////////////////////////////////////////////
-// TO RUN THE ES6 VERSION UNCOMMENT THE LINES BELOW AND COMMENT OUT LINE 12 ABOVE.
-////////////////////////////////////////////////////////////////////////////////////////////////////////
-
 app.use(webpackMiddleware(compiler, {
   quiet: true,
   noInfo: true,
@@ -26,8 +20,5 @@ app.use(webpackMiddleware(compiler, {
 }));
 app.use(webpackHot(compiler));
 app.use(express.static(path.join(__dirname, 'es6Refactor/')));
-
-////////////////////////////////////////////////////////////////////////////////////////////////////////
-
 
 app.listen(process.env.PORT || 3000);
