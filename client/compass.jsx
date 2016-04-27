fb436f9a7a20cf49abba300fb3b218f205a8e941
shindigira <shindigira@gmail.com>
2016-04-26 19:58:45 -0700

    got require/import working on client side

--- client/compass.jsx
+++ client/compass.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 // var geomagnetism = require('geomagnetism');
-// import geomagnetism from 'geomagnetism';
+import geomagnetism from 'geomagnetism';
 
 class Compass extends React.Component {
   constructor(props) {
@@ -11,14 +11,13 @@ class Compass extends React.Component {
     this.init();
   }
   init() {
-    // let info = geomagnetism.model().point([37.787507, -122.399838]);
-    // console.log("COMPONENT", geomagnetism, '----');
-    let magneticDeclination = 13.66;
+    let info = geomagnetism.model().point([37.787507, -122.399838]);
+    console.log("Magnetic declination: ", info.decl, '----');
+    let magneticDeclination = info.decl;
     // let magneticDeclination = info.decl;
     let compass = document.getElementById('compass');
     if (window.DeviceOrientationEvent) {
 
-      // console.log('magnetic declination:', info.decl);
       window.addEventListener('deviceorientation', function(event) {
         var alpha, webkitAlpha;
         //Check for iOS property
@@ -50,6 +49,7 @@ class Compass extends React.Component {
         document.getElementById('heading').innerHTML = "360-alpha: " + ((360 - alpha)%360);
         document.getElementById('heading2').innerHTML = "magneticDeclination+360-alpha: " + ((magneticDeclination+360 - alpha)%360);
         // document.getElementById('heading3').innerHTML = "calculatedBearing: " + (calculatedBearing);
+        document.getElementById('heading4').innerHTML = "Magnetic Declination: " + (magneticDeclination);
       }, false);
     }
   }
@@ -68,6 +68,8 @@ class Compass extends React.Component {
         <img id={'compass'} style={compassStyle} src={'compass5.png'} />
         <div id={'heading'}></div>
         <div id={'heading2'}></div>
+        <div id={'heading3'}></div>
+        <div id={'heading4'}></div>
       </div>
     );
   }
