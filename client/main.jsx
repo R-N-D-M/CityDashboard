3767342a2a1ba4e259c1cf68b2ba6fac3ea242ac
Ramin <ramin_yavari03@yahoo.com>
2016-04-27 19:42:16 -0700

    core: passing current location through main

--- client/main.jsx
+++ client/main.jsx
@@ -18,12 +18,25 @@ class App extends React.Component {
     super(props);
     this.state = {};
   }
+  componentDidMount() {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition( (position) => {
+        console.log(position);
+        let lat = position.coords.latitude;
+        let lng = position.coords.longitude;
+        this.setState({
+          locationTrue: [lat, lng]
+        });
+      });
+    }
+    console.log('Main component mounted');
+  }
   render() {
     return (
       <div style={{height: "100%", width: "100%"}}>
         <Inner />
         <div className="bothcontainer" style={{display: "flex", width: "100%", height: "100%"}}>
-          <div style={{width: "80%", height: "80%", flex: "1", border: "1px solid red"}} id={'widgets'}><Weather/></div>
+          <div style={{width: "80%", height: "80%", flex: "1", border: "1px solid red"}} id={'widgets'}><Weather location={this.state.locationTrue}/></div>
           <div style={{width: "20%", height: "80%", border: "1px solid blue"}} id={'controlPanel'}>Control Panel</div>
         </div>
       </div>
