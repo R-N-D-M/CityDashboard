b62a9a1e3a677feea3482b4614bf11585b9f0ac7
Maksym <maksymgithub@gmail.com>
2016-04-28 15:03:40 -0700

    committing changes before rebasing new pull request

--- client/bart.jsx
+++ client/bart.jsx
@@ -1,4 +1,9 @@
 
+// should display the next train leaving from your current location
+  // outbound
+  // inbound
+// poll google for closest bart station based upon current location
+// 
 import React, { PropTypes } from 'react';
 import axios from 'axios';
 import xmlToJson from './xmlToJson.js';
@@ -12,9 +17,43 @@ class Bart extends React.Component {
     //props.alert("bart");
   }
 
+  getClosestStation(){
+    const R = 6371000; // metres
+    let φ1 = lat1.toRadians();
+    let φ2 = lat2.toRadians();
+    let Δφ = (lat2-lat1).toRadians();
+    let Δλ = (lon2-lon1).toRadians();
+
+    let a = Math.sin(Δφ/2) * Math.sin(Δφ/2) +
+            Math.cos(φ1) * Math.cos(φ2) *
+            Math.sin(Δλ/2) * Math.sin(Δλ/2);
+    let c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
+
+    const d = R * c;
+    return d;
+  }
+  
+  getListOfStations(){
+    const base = 'http://api.bart.gov/api/stn.aspx?cmd=stns&key=MW9S-E7SL-26DU-VV8V';
+    return axios.get(base)
+      .then((response) => {
+        return response.data
+      })
+      .catch((response) => {
+        if (response instanceof Error){
+          console.log('Error: ', response.message);
+        } else {
+          console.log(response.data);
+        }
+      });
+  }
+  
   componentWillMount(){
-    let delays = this.getBartDelays();
-    this.setState({ myValue: delays });
+    console.log('bart component will mount');
+    const stations = getListOfStations();
+    console.log("stations list: ", stations);
+    // let delays = getBartDelays();
+    // this.setState({ myValue: delays });
   }
 
   getBartDelays(){
@@ -43,15 +82,15 @@ class Bart extends React.Component {
       return <div></div>;
     } else {
       return (
-        <div className='bartDelays'>
+        <div className='bart'>
           <p>{this.state.myValue}</p>
         </div>
       );
     }
   }
 }
-// Bart.propTypes = {
+Bart.propTypes = {
 
-// };
+};
 
 export default Bart;
