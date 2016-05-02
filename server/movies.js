4dc31bd0d9ae414e4bd16755315833bb5b0070bc
Ramin <ramin_yavari03@yahoo.com>
2016-05-02 12:23:38 -0700

    Core: rendered movie infro on page

--- server/movies.js
+++ server/movies.js
@@ -1,19 +1,21 @@
 var request = require('request');
 var axios = require('axios');
 
-var Movies_ID = '6gckbwqssxybn76ccsu56nd6';
+// var Movies_ID = '6gckbwqssxybn76ccsu56nd6';
+var Movies_ID = 'dvnxckvy52m4mpeh3zfqpyhc';
+// var Movies_ID = 'qk93ft2mkdfavw8abjr4yy9b';
 
 var getMovies = function(request, response) {
   var url;
   var latLng = request.body.latLong;
   var date = new Date();
+
   var startDate = date.getFullYear() + '-' + (date.getMonth()+1) + '-' + date.getDate();
   url = 'http://data.tmsapi.com/v1.1/movies/showings?startDate=' + startDate + '&lat=' + latLng[0] + '&lng=' + latLng[1] + '&radius=0.5&units=mi&api_key=' + Movies_ID;
-  console.log(url);
-  
+  console.log('this is the url', url)
   axios.get(url)
     .then( (resp) => {
-      console.log("Movies: ",resp.data);
+      // console.log("Movies: ",resp.data);
       sendMovies(resp.data, request, response)
 
     })
@@ -24,8 +26,37 @@ var getMovies = function(request, response) {
 };
 
 var sendMovies = function(data, request, response) {
+  // console.log('this is the data',data);
+
+  data = apiHelper(data);
+
+  console.log('this is the data',data);
 
   response.status(200).send(data);
+}
+
+var apiHelper = function(data) {
+
+  return data.reduce(function(result, curr){
+  var showTimesAtTheatre = curr.showtimes.reduce(function(innerResult, current){
+    var theatreName = current.theatre.name;
+    innerResult[theatreName] = (innerResult[theatreName]) ? innerResult[theatreName].concat(current.dateTime) : [current.dateTime];
+    return innerResult;
+  }, {});
+  
+    // console.log(showTimesAtTheatre)
+    var currentTitle = curr.title;
+    
+    for (var key in showTimesAtTheatre) {
+      var titleShowtimes = {
+        title: currentTitle,
+        showtimes: showTimesAtTheatre[key]
+      };
+      
+      result[key] = (!result[key]) ? [titleShowtimes] : result[key].concat(titleShowtimes);
+    }
+  return result;
+}, {});
 }
 
 
