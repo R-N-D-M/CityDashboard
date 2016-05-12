b41b58cbafae873a86066b37f4bd0043412f443f
Maksym <maksymgithub@gmail.com>
2016-05-11 20:19:55 -0700

    style: more styling to B.A.R.T. widget

--- client/widgets/bart.jsx
+++ client/widgets/bart.jsx
@@ -109,7 +109,7 @@ class Bart extends React.Component {
       } else {
         return (
           <div className='card'>
-            <div className='card-header text-xs-center'>Departing from: {this.state.originStation}</div>
+            <div className='card-header text-xs-center departing'>Departing from: {this.state.originStation}</div>
             <div className='TrainsData'>
               <table className='table table-sm table-responsive table-striped'>
                 <tbody>
